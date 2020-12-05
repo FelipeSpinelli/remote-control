d3a655654a3b42add9715841d6a11384ad3bf39a
Felipe Spinelli <felipespinelli19@gmail.com>
2020-12-05 20:30:31 -0300

    :tada:

---
+++ RemoteControl/RemoteControl.API/Controllers/CommandController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RemoteControl.API.Models;
+using System;
+using System.Diagnostics;
+
+namespace RemoteControl.API.Controllers
+{
+    [ApiController]
+    [Route("commands")]
+    public class CommandController : ControllerBase
+    {
+        private static Process _openedProcess;
+
+        [HttpPost]
+        public IActionResult Send([FromBody] CommandViewModel command)
+        {
+            try
+            {
+                if (_openedProcess != null)
+                {
+                    _openedProcess.Kill();
+                }
+
+                _openedProcess = Process.Start(new ProcessStartInfo(command.Url)
+                {
+                    UseShellExecute = true,
+                    Verb = "open"
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ RemoteControl/RemoteControl.API/Models/CommandViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RemoteControl.API.Models
+{
+    public class CommandViewModel
+    {
+        public string Device { get; set; }
+        public string Url { get; set; }
+    }
+}
